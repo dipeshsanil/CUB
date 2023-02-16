a399419e9f744f9d3a6c686c7345b99fd78ac411
dipeshsanil <dipeshsanil24@gmail.com>
2023-02-16 21:22:15 +0530

    web3storage

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/_VrVP1xGZG277SebNcJre_Vd5SEiRUgC',
+      url: 'https://eth-goerli.g.alchemy.com/v2/S3HNsJ4nS3yq0jZojFJonlOgn9AJFbqq',
       accounts: [`0x6d3c5f5198de403706b0080f54aa1761f31bf4f75a6ddcea18e4893f745360a8`]
     }
   },
