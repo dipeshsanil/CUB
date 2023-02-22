a262c033bfed38907bfec75aea87c5880a84d901
Divesh Sanil <diveshdipesh1@gmail.com>
2023-02-22 18:41:57 +0530

    Update Preview.js

--- src/frontend/Preview.js
+++ src/frontend/Preview.js
@@ -6,29 +6,70 @@ import { ethers } from "ethers";
 import altImg from "./img/file_alt.jpg";
 
 const Preview = ({ upload }) => {
-	const navigate = useNavigate();
-	const location = useLocation();
-	const item = location.state;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const item = location.state;
 
-	useEffect(() => {
-		// onImageChange(item.image);
-	}, []);
+  useEffect(() => {
+    // onImageChange(item.image);
+  }, []);
 
-	return (
-		<div id="details" className="container">
-			<img
-				onError={(e) => {
-					e.target.src = altImg;
-				}}
-				src={item.image}
-				style={{ width: "100%", borderRadius: "25px" }}
-				alt="Preview not available"
-			/>
-			<p>{item.fileName}</p>
-			<p>{item.size}</p>
-			<p>{item.type}</p>
-		</div>
-	);
+  return (
+    <>
+      <div id="details" className="container"></div>
+      <div id="share" className="container-fluid">
+        <div class="row">
+          <div className="col-lg-6">
+            <img
+              onError={(e) => {
+                e.target.src = altImg;
+              }}
+              src={item.image}
+              style={{ width: "100%", borderRadius: "25px" }}
+              alt="Preview not available"
+            />
+          </div>
+          <div className="col-lg-6 share-content">
+            <h2 className="mb-2">Details</h2>
+            {/* <p>
+              <span className="fw-bold">Name: </span>
+              {item.fileName}
+            </p>
+            <p>
+              <span className="fw-bold">Size: </span>
+              {item.size}
+            </p>
+            <p>
+              <span className="fw-bold">File Type:</span>
+              {item.type}
+            </p> */}
+            <table class="table table-borderless">
+              <tbody>
+                <tr>
+                  <td>
+                    <span className="fw-bold">Name: </span>
+                  </td>
+                  <td>{item.fileName}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="fw-bold">Size: </span>
+                  </td>
+                  <td>{item.size}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="fw-bold">File Type: </span>
+                  </td>
+                  <td>{item.type}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 //TODO:
 export default Preview;
